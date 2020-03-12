94c8aefa6b49b78ad352767fa7174651dfd59ea5
Danavishnu <vishnu5522@gmail.com>
2020-03-12 23:57:35 +0530

    changed_netlify

--- server_1/server.js
+++ server_1/server.js
@@ -1,23 +1,21 @@
-// const express = require('express');
-// const connectDB = require('./DB/Conncection');
-// const app = express();
-// var cors=require('cors')
-// const serverless=require('serverless-http');
+const express = require('express');
+const connectDB = require('./DB/Conncection');
+const app = express();
+var cors=require('cors')
+const serverless=require('serverless-http');
 
-// connectDB();
-// app.use(cors());
-// app.use(express.json({ extended: false }));
-// app.use('/api',require('./Api/User'))
-// app.use('/marks', require('./Api/Marks'));
+
+exports.handler = async event => {
+
+    connectDB();
+    app.use(cors());
+    app.use(express.json({ extended: false }));
+    app.use('/api',require('./Api/User'))
+    app.use('/marks', require('./Api/Marks'));
+  }
+// 
 // const Port = 3003;
 
 // // app.listen(Port, () => console.log('Server started'))
 // module.exports.handler=serverless(app)
 
-exports.handler = async event => {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: `Hello ${subject}!`,
-    }
-  }
